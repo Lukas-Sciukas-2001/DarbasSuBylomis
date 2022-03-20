87e0b82c4e9b98279f2d0b2b4696ded86a05aaae
lukas <lukas@DESKTOP-5C2IHTU>
2022-03-20 18:16:54 +0200

    Add project files.

---
+++ DarbasSuBylomis/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarbasSuBylomis
+{
+    internal class failuKlase
+    {
+        public string kelias;
+        public int kiek;
+        private bool arteising;
+
+        public void kurti()
+        {
+            arteising = false;
+            for (int x = 1; x <= kiek; x++)
+            {
+                var rand = new Random();
+                arteising = false;
+                while (arteising == false)
+                {
+                    string failName = "a" + x + ".txt";
+                    if (!File.Exists(failName))
+                    {
+                        string tekstas = "" + rand.Next(2);
+                        File.WriteAllText(failName, tekstas);
+                        arteising = true;
+                    }
+                    else
+                    {
+
+                        arteising = true;
+                    }
+                }
+            }
+
+        }
+        public failuKlase(int kiekKurt)
+        {
+            kiek = kiekKurt;
+        }
+    }
+}
---
+++ DarbasSuBylomis/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DarbasSuBylomis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            failuKlase obj = new failuKlase(10);
+            obj.kurti();
+        }
+    }
+}
