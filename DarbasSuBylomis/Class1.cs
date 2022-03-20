0a7bae933c330a082c654e86ccdae2bde9d9c3fb
Lukas-Sciukas-IST20 <Lukas.Sciukas@stud.svako.lt>
2022-03-20 18:39:47 +0200

    Pabaigta klase su patikrinimu ar failas egzistuoja

--- DarbasSuBylomis/Class1.cs
+++ DarbasSuBylomis/Class1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DarbasSuBylomis
 {
@@ -14,6 +15,12 @@ namespace DarbasSuBylomis
 
         public void kurti()
         {
+            DateTime now = DateTime.UtcNow;
+            int year;
+            int month;
+            int day;
+            string data;
+            string naujasfailName;
             arteising = false;
             for (int x = 1; x <= kiek; x++)
             {
@@ -30,7 +37,18 @@ namespace DarbasSuBylomis
                     }
                     else
                     {
-
+                        year = now.Year;
+                        month = now.Month;
+                        day = now.Day;
+                        data = ""+year + '-' + month + '-' + day;
+                        naujasfailName = "a"+ x+ "_" + data+".txt";
+                        if (File.Exists(naujasfailName))
+                        {
+                            File.Delete(naujasfailName);
+                        }
+                        File.Move(failName, naujasfailName);
+                        string tekstas = "" + rand.Next(2);
+                        File.WriteAllText(failName, tekstas);
                         arteising = true;
                     }
                 }
